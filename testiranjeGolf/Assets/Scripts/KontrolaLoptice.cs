1e01698bd83580b13dd4f4cae1553b21ecaa8344
MistFuror <acastefanovic99@hotmail.com>
2018-07-11 12:02:29 +0200

    Ceo projekat
    Sadrzi samo potrebne foldere za unity projekat, ostale bi sam unity trebao da generise, na ovome radimo

---
+++ testiranjeGolf/Assets/Scripts/KontrolaLoptice.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KontrolaLoptice : MonoBehaviour {
+
+	private LineRenderer linijaPravca;  // Linija indikatora pravca
+
+	private Rigidbody2D rigb;  // 2D telo (fizika) loptice
+
+	bool indikator_dodira = false;
+
+	float startPosX;
+	float krajPosX;
+	float startPosY;
+	float krajPosY;
+
+	float brzina = 3;
+
+	Vector2 tackaPom;
+
+	void Start () {
+		linijaPravca = GetComponent<LineRenderer>();
+		linijaPravca.positionCount = 2;
+		rigb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update () {
+
+	}
+		
+	void OnMouseDown() { 
+		if (rigb.IsSleeping ()) { // IsSleeping proverava da li je loptica potpuno mirna da bi se novi potez mogao otpoceti (posto u poslednjim trenucima usporavanja loptica nastavlja sa gotovo neprimetnim kretanjem, valjalo bi ubaciti proveru u Update () gde ako je brzina loptice (gameObject.Velocity) manja od neke male zadate vrednosti, ona se automatski postavlja na 0 radi manjeg cekanja za zaustavljanje loptice
+			linijaPravca.positionCount = 2;
+			startPosX = Input.mousePosition.x;
+			startPosY = Input.mousePosition.y;
+			indikator_dodira = true;
+		}
+	}
+
+	void OnMouseDrag () { // Potrebno je napraviti tako da indikator bude fiksne duzine, ali i da se odbija o zidove
+		if (rigb.IsSleeping ()) {  
+			Vector2 tackaPom = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			tackaPom.x = gameObject.transform.position.x * 2 - tackaPom.x;
+			tackaPom.y = gameObject.transform.position.y * 2 - tackaPom.y;
+			Vector3 lokacija = tackaPom;
+			linijaPravca.SetPositions (new[] { gameObject.transform.position, lokacija });
+		}
+	}
+
+	void OnMouseUp() {
+    	if (indikator_dodira == true) {
+
+			linijaPravca.positionCount = 0;
+
+			krajPosX = Input.mousePosition.x;
+			krajPosY = Input.mousePosition.y;
+
+			float silaX = startPosX - krajPosX;
+			float silaY = startPosY - krajPosY;
+
+			tackaPom.x = silaX;
+			tackaPom.y = silaY;
+
+			rigb.AddForce(tackaPom * brzina);
+
+			indikator_dodira = false;
+		} 
+
+		else {
+			return;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		// Koristimo u slucaju da loptica dodirne prepreku
+
+
+		//rigb.velocity = Vector2.zero;
+	}
+}
