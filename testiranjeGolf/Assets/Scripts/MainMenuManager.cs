3ced9fb2f7dd7bfd8c00821912d6636bc3768ee8
TheHeadhog <stevan.popovic.headhog@gmail.com>
2018-07-24 18:55:50 +0200

    Funkcije glavnog menija i povratak na glavni meni
    Odradio sam bazicni glavni meni i X dugme u igrici kojime se vraca na glavni meni

---
+++ testiranjeGolf/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenuManager : MonoBehaviour {
+
+    public Button buttonPlay;
+    public Button buttonExit;
+
+    private void Awake()
+    {
+        //Prave se listeneri koji cekaju klik
+        buttonPlay.onClick.AddListener(OnButtonPlayClick);
+        buttonExit.onClick.AddListener(OnButtonExitClick);
+    }
+
+    private void OnButtonPlayClick()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    private void OnButtonExitClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ testiranjeGolf/Assets/Scripts/UIKontrola.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIKontrola : MonoBehaviour {
+
+    public Button buttonX;
+
+    public void Awake()
+    {
+        buttonX.onClick.AddListener(BtnXOnClick);
+    }
+
+    void BtnXOnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
