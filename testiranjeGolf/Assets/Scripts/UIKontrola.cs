d575443dc1f631b791b93e0703dd3ad973f9ce2f
TheHeadhog <stevan.popovic.headhog@gmail.com>
2018-07-25 23:47:24 +0200

    Odradjene finkcije Pause menija

--- testiranjeGolf/Assets/Scripts/UIKontrola.cs
+++ testiranjeGolf/Assets/Scripts/UIKontrola.cs
@@ -3,26 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class UIKontrola : MonoBehaviour {
 
     [SerializeField]
-    private Button buttonX;
-
+    private Button ButtonX;
+    [SerializeField]
+    private Button ButtonPlay;
+    [SerializeField]
+    private Button ButtonExit;
     [SerializeField]
     private Canvas PauseMenu;
 
     public void Awake()
     {
-        buttonX.onClick.AddListener(BtnXOnClick);
+        PauseMenu.enabled = false;
+        ButtonX.onClick.AddListener(PauseToggle);
     }
 
-    void BtnXOnClick()
+    void PauseToggle()
     {
-        if (PauseMenu.enabled == true)
+        if (PauseMenu.enabled == true) //Izlazak iz pauze
+        {
+            ButtonX.gameObject.SetActive(true);
             PauseMenu.enabled = false;
-        else
+            ButtonPlay.onClick.RemoveAllListeners();
+            ButtonExit.onClick.RemoveAllListeners();
+        }
+        else //Ulazak u pauzu
+        {
+            ButtonX.gameObject.SetActive(false);
             PauseMenu.enabled = true;
+            ButtonPlay.onClick.AddListener(PauseToggle);
+            ButtonExit.onClick.AddListener(BtnExtitOnClick);
+        }
     }
 
+    private void BtnExtitOnClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
 }
